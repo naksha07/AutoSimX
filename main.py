f3dbfa5a274a3e2051c0b99fa30bf40a6b4640cc
Nakshathraa N B <nakshathraa.2202141@srec.ac.in>
2026-07-13 22:24:47 +0530

    Add scheduler, logger, vehicle state and project configuration

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-"""
-AutoSimX
-Virtual AUTOSAR ECU Communication Simulator
-"""
-
 from communication.can_bus import CANBus
 
 from ecu.engine_ecu import EngineECU
@@ -10,22 +5,41 @@ from ecu.cluster_ecu import ClusterECU
 from ecu.body_ecu import BodyECU
 from ecu.gateway_ecu import GatewayECU
 
+from scheduler.task_scheduler import TaskScheduler
+
 
 def main():
 
+    print("=" * 60)
+    print("        AutoSimX - Automotive ECU Simulator")
+    print("=" * 60)
+
+    # Create CAN Bus
     bus = CANBus()
 
+    # Create ECUs
     engine = EngineECU()
     cluster = ClusterECU()
     body = BodyECU()
     gateway = GatewayECU()
 
+    # Register ECUs
     bus.register_ecu(engine)
     bus.register_ecu(cluster)
     bus.register_ecu(body)
     bus.register_ecu(gateway)
 
-    engine.simulate_drive()
+    # Create Scheduler
+    scheduler = TaskScheduler()
+
+    # Add ECU Tasks
+    scheduler.add_task(engine.simulate_drive)
+
+    # Start all tasks
+    scheduler.start()
+
+    # Wait for all tasks to finish
+    scheduler.wait()
 
 
 if __name__ == "__main__":
